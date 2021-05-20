2f108dc5dc858e17b98abece5e5c8a499d45cf57
eric lyimo <riclyimo29@gmail.com>
2021-05-20 08:46:57 +0300

    fifth commit

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -5,14 +5,29 @@ import Styled from 'styled-components/native'
 import {RestaurantContext} from '../../../services/restaurants/restaurants.context'
 import { RestaurantInfoCard } from "../components/restaurant-info.component";
 import {SafeArea} from "../../../utils/safe-area.component"
+import {ActivityIndicator,Colors} from "react-native-paper"
+
 const SearchView=Styled(View) `padding: ${(props)=>props.theme.sizes[1]};`
 
 const ViewList = Styled(View) ` flex: 1; padding: ${(props)=>props.theme.sizes[1]}; background-color: ${(props)=>props.theme.colors.ui.tertiary}; `
+
+const Loading = Styled(View)`position:absolute; left:50%; top:50%`
 
 export const RestaurantsScreen = () => {
   const {restaurants,isLoading} = useContext(RestaurantContext)
   return(
     <SafeArea >
+      {isLoading && (
+        <Loading>
+          <ActivityIndicator>
+            size={50},
+            color={Colors.blue},
+            style={{marginLeft:-25}}
+            animating={true}
+          </ActivityIndicator>
+        </Loading>
+       )
+      }
     <SearchView>
       <Searchbar />
     </SearchView>
